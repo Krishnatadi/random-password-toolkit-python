6aa2802c5d54255dda0ac7b96c30d3cbe791357d
Krishna Tadi <tadikrishna@outlook.com>
2026-01-09 18:14:04 +0530

    security fixes done

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="random-password-toolkit",
-    version="1.0.1",
+    version="1.0.2",
     author="krishna Tadi",
     description="random-password-toolkit is a robust Python package for generating and managing random passwords with advanced features, including encryption, decryption, strength checking, and customizable generation options. This package is ideal for Python developers looking for a secure and feature-rich solution for handling password-related tasks.",
     long_description=open("README.md").read(),
@@ -16,7 +16,7 @@ setup(
     ],
     python_requires=">=3.6",
     install_requires=[
-        "cryptography==38.0.0"
+        "cryptography>=38.0.0"
     ],
     keywords='""random password generator", "password strength checker", "password encryption", "password decryption", "secure password management", "customizable password generation", "Python password toolkit", "Random number generation"',
     project_urls={
